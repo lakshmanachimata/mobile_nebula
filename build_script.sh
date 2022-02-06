58c8c72869c5bdf908f8d0d4e09ea104f8464b26
lakshmana <lakshman.chimata@gmail.com>
2022-02-06 13:53:28 +0530

    added build script

---
+++ build_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -rf ./build
+rm ./nebula/mobileNebula-sources.jar
+rm ./nebula/mobileNebula.aar
+flutter build appbundle --debug  --verbose
+java -jar ~/projects/nearhop/bundletool.jar build-apks --mode=universal  --bundle=./build/app/outputs/bundle/debug/app-debug.aab  --output=./build/app/outputs/bundle/debug/app-debug.apks 
